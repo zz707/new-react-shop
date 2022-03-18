56a100f5b762e86013c47728efdfff066ee43740
zz707 <zarinmbox@gmail.com>
2022-03-18 15:32:28 +0300

    Add Cart component.

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -2,10 +2,12 @@ import {useEffect, useState} from "react";
 import {API_KEY, API_URL} from "../config";
 import {Preloader} from "./Preloader";
 import {GoodsList} from "./GoodsList";
+import {Cart} from "./Cart";
 
 function Shop() {
     const [goods, setGoods] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [order, setOrder] = useState([]);
 
     useEffect(function getGoods() {
         fetch(API_URL, {
@@ -21,6 +23,7 @@ function Shop() {
     }, []);
 
     return <main className="container content">
+        <Cart quantity={order.length} />
         {loading ? <Preloader /> : <GoodsList goods={goods} />}
     </main>
 }
