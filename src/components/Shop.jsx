e7598112a48d349bedbb8c7a68732e96af51f1bf
zz707 <zarinmbox@gmail.com>
2022-03-18 14:46:43 +0300

    Add Shop, GoodsList and GoodsItem components.

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,5 +1,28 @@
+import {useEffect, useState} from "react";
+import {API_KEY, API_URL} from "../config";
+import {Preloader} from "./Preloader";
+import {GoodsList} from "./GoodsList";
+
 function Shop() {
-    return <main className="container content"></main>
+    const [goods, setGoods] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(function getGoods() {
+        fetch(API_URL, {
+            headers: {
+                'Authorization': API_KEY,
+            }
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                data.featured && setGoods(data.featured);
+                setLoading(false);
+            })
+    }, []);
+
+    return <main className="container content">
+        {loading ? <Preloader /> : <GoodsList goods={goods} />}
+    </main>
 }
 
 export {Shop}
